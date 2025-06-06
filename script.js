a9f0f761b9e363acfe03daf2e803b472051ee3b1
Hridoy5Chowdhury <hridoychowdhury365@gmail.com>
2025-06-06 10:11:56 +0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const button = "Added a Button";
+console.log(button);
+
+const login = "Login page added";
+console.log(login);
+
+const footer = "Footer added in our website";
+console.log(footer);
+const Payment = "Integrated the payment gateway";
+console.log(Payment);
+
+const upi = "Integrated the UPI";
+console.log(upi);
